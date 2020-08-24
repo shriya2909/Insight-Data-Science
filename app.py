2484a70625934934afbb3dea87829d2200a169f3
Shriya Rai <44680091+shriya2909@users.noreply.github.com>
2020-08-24 14:14:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import yake
 from langdetect import detect
 import iso_language_codes as ilc
 from googletrans import Translator
+from multi_rake import Rake
 
 
 st.title("TL;DR")
@@ -36,6 +37,15 @@ if uploaded_file is not None:
     lang = detect(txt)
     str1 = "Detected Origin of language : " + ilc.language_name(lang)
     st.write(str1)
+    
+    #----- RAKE 
+    rake = Rake(language_code='es', max_words=1)
+    rakekeywords = rake.apply(txt)
+
+    if len(rakekeywords) > 25 :
+        rakekeywords = rakekeywords[:25]
+        
+    #----- YAKE 
     max_ngram_size = 3
     deduplication_thresold = 0.9
     deduplication_algo = 'seqm'
@@ -43,15 +53,32 @@ if uploaded_file is not None:
     numOfKeywords = 25
     
     custom_kw_extractor = yake.KeywordExtractor(lan=lang, n=max_ngram_size, dedupLim=deduplication_thresold, dedupFunc=deduplication_algo, windowsSize=windowSize, top=numOfKeywords, features=None)
-    keywords = custom_kw_extractor.extract_keywords(txt)
+    yakekeywords = custom_kw_extractor.extract_keywords(txt)
     st.write("Extracting keywords now ...\n")
     
     translator = Translator()
     translated = translator.translate('Keywords', src='en', dest= lang)
     st.subheader(translated.text)
     
-    for kw in keywords:
+    for kw in yakekeywords:
         st.write(kw[1])
+    tot_kw =[]
+    for i in range(len(yakekeywords)):
+        tot_kw.append(yakekeywords[i][0])
+        
+    for i in range(len(rakekeywords)):
+        tot_kw.append(rakekeywords[i][0])
+               
+          
+    freq = {} 
+     for items in tot_kw: 
+            freq[items] = tot_kw.count(items) 
+            
+    sorted_x = sorted(freq.items(), key=lambda kv: kv[1], reverse=True)
+    for kw in sorted_x:
+        st.write(kw[1])
+             
+    
 
 
 
